377f6e3839458b598bb07dc1de854e460dfbb912
Abdul Rehman <khanzadaabdulrehman1@gmail.com>
2023-03-23 00:32:41 +0500

    created a daynamic table in js

--- app.js
+++ app.js
@@ -5,37 +5,35 @@
 //     { name: 'Sir Atif', age: 23, subject: 'Urdu' }
 // ];
 
-
 const students = [
-  { name: 'Ali', age: 15, class: '8th' },
-  { name: 'Taha', age: 19, class: '9th' },
-  { name: 'Saad', age: 20, class: '10th' },
-  { name: 'Saad', age: 20, class: '10th' }
+  { name: "Ali", age: 15, class: "8th" },
+  { name: "Taha", age: 19, class: "9th" },
+  { name: "Saad", age: 20, class: "10th" },
 ];
 
 function renderTable() {
-  const table = document.getElementById('table');
-  const headerRow = document.createElement('tr');
-  Object.keys(students[0]).forEach(key => {
-    const headerCell = document.createElement('th');
-    headerCell.textContent = key;
-    headerRow.appendChild(headerCell);
+  const table = document.getElementById("table");
+  const tableBody = document.createElement("tbody")
+  table.className = "table table-striped";
+  const headerRow = document.createElement("tr");
+  Object.keys(students[0]).forEach((key) => {
+      const headerCell = document.createElement("th");
+      headerCell.className = "text-center p-2";
+      headerCell.textContent = key;
+      headerRow.appendChild(headerCell);
   });
   table.appendChild(headerRow);
-  students.forEach(student => {
-    const row = document.createElement('tr');
-    Object.keys(student).forEach(key => {
-      const cell = document.createElement('td');
-      cell.textContent = student[key];
-      row.appendChild(cell);
-    });
-    table.appendChild(row);
+  students.forEach((student) => {
+      const row = document.createElement("tr");
+      row.className = "text-center  ";
+      Object.keys(student).forEach((key) => {
+          const cell = document.createElement("td");
+          cell.className = "p-2  ";
+          cell.textContent = student[key];
+          row.appendChild(cell);
+          tableBody.appendChild(row)
+      });
+      table.appendChild(tableBody);
   });
 }
 renderTable();
-
-
-
-
- 
-  
