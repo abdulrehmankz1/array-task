8652d000a7a293e69fc6a8c3b727b32e9a05d8aa
Abdul Rehman <khanzadaabdulrehman1@gmail.com>
2023-03-22 15:40:24 +0500

    create a table with the help of js

---
+++ app.js
@@ -0,0 +1,41 @@
+// // create a teacher array
+// const teachers = [
+//     { name: 'Sir Ahmed', age: 21, subject: 'English' },
+//     { name: 'Sir Rehan', age: 22, subject: 'Math' },
+//     { name: 'Sir Atif', age: 23, subject: 'Urdu' }
+// ];
+
+
+const students = [
+  { name: 'Ali', age: 15, class: '8th' },
+  { name: 'Taha', age: 19, class: '9th' },
+  { name: 'Saad', age: 20, class: '10th' },
+  { name: 'Saad', age: 20, class: '10th' }
+];
+
+function renderTable() {
+  const table = document.getElementById('table');
+  const headerRow = document.createElement('tr');
+  Object.keys(students[0]).forEach(key => {
+    const headerCell = document.createElement('th');
+    headerCell.textContent = key;
+    headerRow.appendChild(headerCell);
+  });
+  table.appendChild(headerRow);
+  students.forEach(student => {
+    const row = document.createElement('tr');
+    Object.keys(student).forEach(key => {
+      const cell = document.createElement('td');
+      cell.textContent = student[key];
+      row.appendChild(cell);
+    });
+    table.appendChild(row);
+  });
+}
+renderTable();
+
+
+
+
+ 
+  
